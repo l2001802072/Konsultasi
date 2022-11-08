60c7787879f079d7f637aaa1fa716c4814bd2ab2
Naura Fika <55442776+l2001802072@users.noreply.github.com>
2022-11-08 09:42:01 +0700

    Add files via upload

---
+++ beranda.php
@@ -0,0 +1,11 @@
+<div class="box box-success">
+    <div class="box-header with-border">
+        <h3 class="box-title">Selamat Datang!</h3>
+    </div>
+    <div class="box-body content-custom">
+    	<h2 style="text-align: center;">Konsultasikan Jerawat anda disini!</h2>
+    	<p class="lead" style="text-align: center;"><br/>
+        <img src="assets/images/j.jpg">
+    	</p>
+    </div>
+</div>
